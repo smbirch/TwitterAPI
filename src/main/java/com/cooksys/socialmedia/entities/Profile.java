fca8c38f923ad647bce7f0e1d6530f0e12e7ccf4
Matthew Oshimo <moshimo90696@gmail.com>
2024-01-29 22:53:14 -0800

    did profile infrasctructure

---
+++ src/main/java/com/cooksys/socialmedia/entities/Profile.java
@@ -0,0 +1,22 @@
+package com.cooksys.socialmedia.entities;
+
+import jakarta.annotation.Nonnull;
+import jakarta.persistence.Embeddable;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Embeddable
+@NoArgsConstructor
+@Data
+public class Profile {
+  
+  private String firstName;
+  
+  private String lastName;
+  
+  @Nonnull
+  private String email;
+  
+  private String phone;
+
+}
