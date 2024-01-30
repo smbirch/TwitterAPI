829e3e39b13653500e3d31f4727d391c9e417267
Matthew Oshimo <moshimo90696@gmail.com>
2024-01-29 21:03:47 -0800

    addded Tweet entity

---
+++ src/main/java/com/cooksys/socialmedia/entities/Tweet.java
@@ -0,0 +1,37 @@
+package com.cooksys.socialmedia.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.sql.Timestamp;
+import java.util.*;
+
+@Entity
+@NoArgsConstructor
+@Data
+public class Tweet {
+
+  @Id
+  @GeneratedValue
+  private Long id;
+
+  private User Author;
+  
+  private Timestamp posted;
+  
+  private String content;
+  
+  @OneToOne
+  @JoinColumn(name = "inReplyTo")
+  private Tweet inReplyTo;
+  
+  @OneToOne
+  @JoinColumn(name = "repostOf")
+  private Tweet repostOf;
+}
