1ebbf8265ae73e01d3cce977009201964dfec6d4
danielnathaniel <danielyaghoobian@gmail.com>
2024-01-30 18:46:17 -0800

    completed controller

---
+++ src/main/java/com/cooksys/socialmedia/controllers/HashtagController.java
@@ -0,0 +1,38 @@
+package com.cooksys.socialmedia.controllers;
+
+
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cooksys.socialmedia.dtos.HashtagResponseDto;
+import com.cooksys.socialmedia.dtos.TweetResponseDto;
+import com.cooksys.socialmedia.services.HashtagService;
+
+
+@RestController
+@RequestMapping("/tags")
+public class HashtagController {
+
+    private final HashtagService hashtagService;
+
+    public HashtagController(HashtagService hashtagService) {
+        this.hashtagService = hashtagService;
+    }
+
+    @GetMapping
+    public List<HashtagResponseDto> retrieveAllTags() {
+    		return hashtagService.getAllTags();
+    }
+
+    @GetMapping("/{label}")
+    public List<TweetResponseDto> retrieveTweetsBylabel(@PathVariable String label) {	
+    		return hashtagService.getTweetsByTag(label);
+    }
+
+
+}
