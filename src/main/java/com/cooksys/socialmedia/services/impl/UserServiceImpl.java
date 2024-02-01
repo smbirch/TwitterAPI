32734dc6466a24161d7448f01944be4139d389a8
danielnathaniel <danielyaghoobian@gmail.com>
2024-01-31 17:19:38 -0800

    created get users endpoint

--- src/main/java/com/cooksys/socialmedia/services/impl/UserServiceImpl.java
+++ src/main/java/com/cooksys/socialmedia/services/impl/UserServiceImpl.java
@@ -6,6 +6,10 @@ import org.springframework.stereotype.Service;
 
 import com.cooksys.socialmedia.dtos.UserRequestDto;
 import com.cooksys.socialmedia.dtos.UserResponseDto;
+import com.cooksys.socialmedia.mappers.HashtagMapper;
+import com.cooksys.socialmedia.mappers.UserMapper;
+import com.cooksys.socialmedia.repositories.HashtagRepository;
+import com.cooksys.socialmedia.repositories.UserRepository;
 import com.cooksys.socialmedia.services.UserService;
 
 import lombok.RequiredArgsConstructor;
@@ -13,9 +17,11 @@ import lombok.RequiredArgsConstructor;
 @Service
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
+	private final UserRepository userRepository;
+	private final UserMapper userMapper;
     @Override
     public List<UserResponseDto> getAllUsers() {
-        return null;
+    		return userMapper.entitiesToDtos(userRepository.findAll());
     }
 
     @Override
