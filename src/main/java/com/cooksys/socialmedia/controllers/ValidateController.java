26a9bfbab205e94b28b3067f9cfce32b82ae56dd
danielnathaniel <danielyaghoobian@gmail.com>
2024-02-01 18:59:23 -0800

    implemented endpoint for GET validate/tag/exists/label

--- src/main/java/com/cooksys/socialmedia/controllers/ValidateController.java
+++ src/main/java/com/cooksys/socialmedia/controllers/ValidateController.java
@@ -17,7 +17,7 @@ public class ValidateController {
 
     //    Checks whether a given hashtag exists.
     @GetMapping("/tag/exists/{label}")
-    public boolean checkForHashtagExistance(@PathVariable String label) {
+    public boolean checkForHashtagExistance(@PathVariable("label") String label) {
         return validateService.checkForHashtagExistance(label);
     }
 
