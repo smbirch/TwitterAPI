f7b6c93bbe53de55e3e251fd55109b8c01ac8dbf
danielnathaniel <danielyaghoobian@gmail.com>
2024-02-01 19:20:21 -0800

    added missing file

--- src/main/java/com/cooksys/socialmedia/repositories/HashtagRepository.java
+++ src/main/java/com/cooksys/socialmedia/repositories/HashtagRepository.java
@@ -1,10 +1,16 @@
 package com.cooksys.socialmedia.repositories;
 
 import com.cooksys.socialmedia.entities.Hashtag;
+import com.cooksys.socialmedia.entities.User;
+
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface HashtagRepository extends JpaRepository<Hashtag, Long> {
+
+    Optional<Hashtag> findByLabel(String label);
 
 }
